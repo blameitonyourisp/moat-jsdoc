5c8d88daa55c2a0fc41cbe159fa8ea7e65896037
James Reid <jamesreid3806@gmail.com>
2023-02-14 15:30:58 +0000

    MOAT_SAMPLE Create temporary ticket Z8I-2MS
    Create temporary ticket Z8I-2MS which will be deleted from its
    file, but the file itself will not be deleted to simulate a
    completed ticket being deleted from the source file.

---
+++ src-sample/feature.js
@@ -0,0 +1,12 @@
+/**
+ * @function
+ * 
+ * @moat {t_feature}
+ * Z8I-2MS
+ * Temporary ticket
+ * This ticket will be deleted from inside the file, but the file itself will
+ * not be deleted
+ */
+function feature() {
+    // here is a really great feature implementation in the sample codebase...
+}
