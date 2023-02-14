1095dbbecd8a8015c90e25648f0516d263e7e9e9
James Reid <jamesreid3806@gmail.com>
2023-02-14 14:23:22 +0000

    MOAT_SAMPLE Create ticket A32-FJ9
    Create sample moat ticket A32-FJ9 with sample information regarding
    the use of the moat ticket body. This ticket is not intended to be
    changed.

---
+++ src-sample/index.js
@@ -0,0 +1,21 @@
+/**
+ * @file Sample codebase index file
+ * @author James Reid
+ * 
+ * @moat {t_feature|r_moat-v1|p_1|d_2023-02-21|s_open|b_master}
+ * A32-FJ9
+ * Here is the todo title
+ * Here is the todo body... Everything from here until the end of the comment
+ * or the next jsdoc tag is considered to be the body of the moat todo message/
+ * ticket.
+ * 
+ * Everything in the body is considered as gfm markdown with hard line breaks
+ * see the following links for more:
+ *      - https://github.github.com/gfm/
+ *      - https://marked.js.org/
+ * 
+ * This means we can also use task lists as follows:
+ *      - [ ] task not completed
+ *      - [x] completed task
+ * 
+ */
