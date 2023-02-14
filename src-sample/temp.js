78497f0ee64b029754482c92534030bcc340eb24
James Reid <jamesreid3806@gmail.com>
2023-02-14 15:25:38 +0000

    MOAT_SAMPLE Change ticket body EI3-491
    Change body of temporary ticket EI3-491 adding a task list for the
    temp function

--- src-sample/temp.js
+++ src-sample/temp.js
@@ -4,7 +4,9 @@
  * @moat {t_feature|p_0|s_open|b_master}
  * EI3-491
  * Temporary ticket to be deleted
- * This function needs a return statement
+ * Add functionality to this function:
+ *      - [ ] add return statement
+ *      - [ ] validate function inputs
  */
 function temp() {
     // this is a temporary file that will later be deleted
