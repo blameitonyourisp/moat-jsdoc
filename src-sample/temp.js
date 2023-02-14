bc88cb09f45d16263db60acaaae867062aa30e71
James Reid <jamesreid3806@gmail.com>
2023-02-14 15:22:24 +0000

    MOAT_SAMPLE Create temporary ticket EI3-491
    Create temporary ticket that will be changed and then deleted from
    the sample codebase.

---
+++ src-sample/temp.js
@@ -0,0 +1,11 @@
+/**
+ * @function
+ * 
+ * @moat {t_feature|s_open|b_master}
+ * EI3-491
+ * Temporary ticket to be deleted
+ * This function needs a return statement
+ */
+function temp() {
+    // this is a temporary file that will later be deleted
+}
