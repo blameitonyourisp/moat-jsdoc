167858043d105fcaa67951e6b46fa85ba0f847ac
James Reid <jamesreid3806@gmail.com>
2023-02-15 18:11:26 +0000

    MOAT_SAMPLE Move bug ticket TPD-6N6
    Move bug ticket TPD-6N6 to the correct file in order to simulate
    tickets being moved across files.

--- src-sample/bug.js
+++ src-sample/bug.js
@@ -11,6 +11,11 @@
  * Sample first bug
  * Here is a moat todo for our first bug, now it has been moved to the correct 
  * place in the file, and the body has been changed to reflect
+ * 
+ * @moat {t_bug}
+ * TPD-6N6
+ * Bug first from feature file
+ * This ticket has now been moved to the correct location from the feature file
  */
 function bug() {
     // this function unfortunately has a bug
--- src-sample/feature.js
+++ src-sample/feature.js
@@ -12,12 +12,6 @@
  * Here is the body of a feature ticket which will be changed multiple commits 
  * after its creation:
  *      - [ ] here is the first uncompleted task that need to be resolved
- * 
- * @moat {t_bug}
- * TPD-6N6
- * Bug first from feature file
- * This ticket is a bug ticket that belongs in the bug file along with the bug
- * function.
  */
 function feature() {
     // here is a really great feature implementation in the sample codebase...
