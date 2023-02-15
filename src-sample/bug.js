32cf64ebf9963bbe84db7f039f00b03a20e3a467
James Reid <jamesreid3806@gmail.com>
2023-02-15 17:50:53 +0000

    MOAT_SAMPLE Move bug ticket ICS-8EM
    Move bug ticket ICS-8EM to the correct place in its file,
    simulating a ticket which has been moved within a file to a
    different location.

--- src-sample/bug.js
+++ src-sample/bug.js
@@ -1,15 +1,16 @@
 /**
  * @file
  * 
- * @moat {t_bug}
- * ICS-8EM
- * Sample first bug
- * Here is a moat todo for our first bug, currently it is top level in the 
- * bug file
  */
 
 /**
  * @function
+ * 
+ * @moat {t_bug}
+ * ICS-8EM
+ * Sample first bug
+ * Here is a moat todo for our first bug, now it has been moved to the correct 
+ * place in the file, and the body has been changed to reflect
  */
 function bug() {
     // this function unfortunately has a bug
