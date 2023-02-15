bbdeccd077f5cd66669788ef2e87bc1f40444eb4
James Reid <jamesreid3806@gmail.com>
2023-02-15 17:47:17 +0000

    MOAT_SAMPLE Create bug ticket ICS-8EM
    Create bug ticket ICS-8EM in the top level file comment of the bug
    file. This bug ticket should be moved within this file to the bug
    function.

---
+++ src-sample/bug.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * 
+ * @moat {t_bug}
+ * ICS-8EM
+ * Sample first bug
+ * Here is a moat todo for our first bug, currently it is top level in the 
+ * bug file
+ */
+
+/**
+ * @function
+ */
+function bug() {
+    // this function unfortunately has a bug
+}
